b1239b948d105825f6b4abdf8de381eaecf3e68a
KPHong <kphong16@daum.net>
2024-08-11 01:39:25 +0900

    new app.js file

---
+++ app.js
@@ -0,0 +1,23 @@
+var items = [
+  {
+    name: '연필',
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: '공책',
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: '지우개',
+    price: 500,
+    quantity: 0
+  }
+]
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: items
+  }
+})
