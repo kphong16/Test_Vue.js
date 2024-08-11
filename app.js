13c68eb4eb94e22de4cde85d7fa0fbae57e6fee5
KPHong <kphong16@daum.net>
2024-08-11 14:43:30 +0900

    edit code

--- app.js
+++ app.js
@@ -2,22 +2,45 @@ var items = [
   {
     name: '연필',
     price: 300,
-    quantity: 0
+    quantity: 15
   },
   {
     name: '공책',
     price: 400,
-    quantity: 0
+    quantity: 30
   },
   {
     name: '지우개',
     price: 500,
-    quantity: 0
+    quantity: 20
   }
-]
+];
 var vm = new Vue({
   el: '#app',
   data: {
-    items: items
+    items: items,
+    loggedInButton: '로그인 후에 구매 가능합니다',
+  },
+  filters: {
+    numberWithDelimiter: function(value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: {
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.10)
+    },
+    canBuy: function() {
+      return this.totalPrice >= 1000
+    }
   }
-})
+});
+window.vm = vm
